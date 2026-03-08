cc6cfbe459cd24ffc3c62320ccdb68a451373ef2
Ayush Bansal <bansalayuayu151185@gmail.com>
2026-03-08 08:41:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -1,5 +1,13 @@
 class Solution {
+    // Time Complexity : O(n)
     public String findDifferentBinaryString(String[] nums) {
-        
+        int n = nums.length;
+        StringBuilder result = new StringBuilder("");
+        for(int i=0;i<n;i++){
+            char ch = nums[i].charAt(i) == '0' ? '1':'0';
+            result.append(ch);
+        }
+        return result.toString();
     }
+    
 }
