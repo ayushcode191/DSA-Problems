2bb367f5b07581bc20fdf5288565b8837dbb0216
Ayush Bansal <bansalayuayu151185@gmail.com>
2026-06-16 14:33:47 +0530

    Time: 4 ms (76.47%), Space: 55.5 MB (28.68%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(Character c : s.toCharArray()){
+            if(Character.isLowerCase(c)){
+                sb.append(c);
+            }
+            else if(c == '*'){
+                if(sb.length()>0){
+                    sb.deleteCharAt(sb.length()-1);
+                }
+            }
+            else if(c == '#'){
+                if(sb.length()>0){
+                    sb.append(sb);
+                }
+            }
+            else if(c == '%'){
+                sb.reverse();
+            }
+        }
+        return sb.toString();
+    }
+}
